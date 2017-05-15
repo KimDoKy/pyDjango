86f0b10ded1a9e55f9c56e2980131c03bf4e482b
Kim Do Ky <makingfunk0@gmail.com>
2017-05-16 02:46:21 +0900

    add home views

--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-# from bookmark.views import BookmarkLV, BookmarkDV
-from bookmark.models import Bookmark
+from mysite.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
 
-    # Class-based views for Bookmark app
-    # url(r'^bookmark/$', BookmarkLV.as_view(model=Bookmark), name='index'),
-    # url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(model=Bookmark), name='detail'),
 ]
