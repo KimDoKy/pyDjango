323c41b564719a7a9934898b5ddfd95e0a8abfeb
Kim Do Ky <makingfunk0@gmail.com>
2017-05-20 00:31:15 +0900

    add chapter13 / content 편집(photo app)

--- ch2/photo/models.py
+++ ch2/photo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from photo.fields import ThumbnailImageField
+from django.contrib.auth.models import User
 
 class Album(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['name']
@@ -13,7 +15,7 @@ class Album(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('photo:album_detail', args=(self.id))
+        return reverse('photo:album_detail', args=(self.id,))
 
 class Photo(models.Model):
     album = models.ForeignKey(Album)
@@ -21,6 +23,7 @@ class Photo(models.Model):
     image = ThumbnailImageField(upload_to='photo/%Y/%m')
     description = models.TextField('Photo Description', blank=True)
     upload_date = models.DateTimeField('Upload Date', auto_now_add=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['title']
--- ch2/photo/urls.py
+++ ch2/photo/urls.py
@@ -15,4 +15,28 @@ urlpatterns = [
 
     # Ex: /photo/99/
     url(r'^photo/(?P<pk>\d+)/$', PhotoDV.as_view(model=Photo), name='photo_detail'),
+
+    # Ex: /album/add/
+    url(r'^album/add/$', AlbumPhotoCV.as_view(), name="album_add"),
+
+    # Ex: /album/change/
+    url(r'^album/change/$', AlbumChangeLV.as_view(), name="album_change"),
+
+    # Ex: /album/99/update/
+    url(r'^album/(?P<pk>[0-9]+)/update/$', AlbumPhotoUV.as_view(), name="album_update"),
+
+    # Ex: /album/99/delete/
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', AlbumDeleteView.as_view(), name="album_delete"),
+
+    # Ex: /photo/add/
+    url(r'^photo/add/$', PhotoCreateView.as_view(), name="photo_add"),
+
+    # Ex: /photo/change/
+    url(r'^photo/change/$', PhotoChangeLV.as_view(), name="photo_change"),
+
+    # Ex: /photo/99/update/
+    url(r'^photo/(?P<pk>[0-9]+)/update/$', PhotoUpdateView.as_view(), name="photo_update"),
+
+    # Ex: /photo/99/delete/
+    url(r'^photo/(?P<pk>[0-9]+)/delete/$', PhotoDeleteView.as_view(), name="photo_delete"),
 ]
