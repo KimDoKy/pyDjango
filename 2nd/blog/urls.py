0fb4dc42b8a77ea97acc0749ff0e314046cabc59
Kim Do Ky <makingfunk0@gmail.com>
2017-09-02 18:31:13 +0900

    modify2 ch3 blog url

--- 2nd/blog/urls.py
+++ 2nd/blog/urls.py
@@ -18,10 +18,10 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/$', PostYAV.as_view(), name='post_year_archive'),
 
     # ex: /2017/nov/
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3}/$', PostMAV.as_view(), name='post_month_archive'),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', PostMAV.as_view(), name='post_month_archive'),
 
     # ex: /2017/nov/10/
-    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3}/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
+    url(r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\d{1,2})/$', PostDAV.as_view(), name='post_day_archive'),
 
     # ex: /today/
     url(r'^today/$', PostTAV.as_view(), name='post_today_archive'),
