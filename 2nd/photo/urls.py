6f5ff71e2d8112cb97c46346923442d381f79992
Kim Do Ky <makingfunk0@gmail.com>
2017-09-23 17:42:37 +0900

    add ch13 url

--- 2nd/photo/urls.py
+++ 2nd/photo/urls.py
@@ -13,4 +13,28 @@ urlpatterns = [
 
     # ex: /photo/99/
     url(r'^photo/(?P<pk>\d+)/$', PhotoDV.as_view(), name='photo_detail'),
+
+    # ex: /album/add/
+    url(r'^album/add/$', AlbumPhotoCV.as_view(), name='album_add'),
+
+    # ex: /album/change/
+    url(r'^album/change/$', AlbumChangeLV.as_view(), name='album_change'),
+
+    # ex: /album/99/update/
+    url(r'^album/(?P<pk>[0-9]+)/update/$', AlbumPhotoUV.as_view(), name='album_update'),
+
+    # ex: /album/99/delete/
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', AlbumDeleteView.as_view(), name='album_delete'),
+
+    # ex: /photo/add/
+    url(r'^photo/add/$', PhotoCreateView.as_view(), name='photo_add'),
+
+    # ex: /photo/change/
+    url(r'^photo/change/$', PhotoChangeLV.as_view(), name='photo_change'),
+
+    # ex: /photo/99/update/
+    url(r'^photo/(?P<pk>[0-9]+)/update/$', PhotoUpdateView.as_view(), name='photo_update'),
+
+    # ex: /photo/99/delete/
+    url(r'^photo/(?P<pk>[0-9]+)/delete/$', PhotoDeleteView.as_view(), name='photo_delete'),
 ]
