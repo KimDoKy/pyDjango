0ffb4997337f63c3d4b485d76712c6c515c8cd43
Kim Do Ky <makingfunk0@gmail.com>
2017-10-03 01:15:59 +0900

    add ch14 models, migrate (1:1)

--- 2nd/photo/models.py
+++ 2nd/photo/models.py
@@ -44,3 +44,18 @@ class Photo(models.Model):
 
     def get_absolute_url(self):
         return reverse('photo:photo_detail', args=(self.id,))
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(Place)
+    name = models.CharField(max_length=50, default='DefRestName')
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.name
